18871039a607a41c227d406efb4951089073c891
Slezii <kub.sle@gmail.com>
2025-03-26 23:01:43 +0100

    Added viewmodel

---
+++ src/QuickScreenShoter/QuickScreenShoter.Core/ViewModels/Interfaces/IPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickScreenShoter.Core.ViewModels.Interfaces
+{
+    internal interface IPageViewModel : INotifyPropertyChanged
+    {
+        public bool IsActive { get; set; }
+    }
+}
